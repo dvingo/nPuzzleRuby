687fc0d923ecf5c01a3ce5af90d8e40cd12adc69
Jonathan McKenzie <jonathan.t.mckenzie@gmail.com>
2011-04-20 20:28:35 -0400

    Almost implemented sliding of the nil block and getting next states

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,30 +2,86 @@ require_relative '../../mvGraph/lib/mvGraph.rb'
 class Grid < Vertex
   include Enumerable
   attr_reader :x, :y
-
+  attr_reader :rows
   def initialize(x, y, blocks)
     @x = x
     @y = y
     @rows = Array.new(@y) { Array.new }
     k = 0
+    @block_to_loc = Hash.new
     blocks.each_with_index do |block, i|
       # increment y (k) if x (i) is at the end of the row
       k += 1 if i % @x == 0 and i != 0 
       @rows[k] << block 
+      @block_to_loc[block] = [i % @x, k]
     end
   end
 
   def block(x, y)
-    @rows[y][x] unless @rows.size()-1 < y or @rows[y].size() - 1 < x or y < 0 or x < 0
+    @rows[y][x] unless @rows.size() - 1 < y or @rows[y].size() - 1 < x or y < 0 or x < 0
   end
 
+  # get the x, y for the nil block and move all surrounding squares into it
+  # for the next states
   def next_states
     states = []
-    # get the x, y for the nil block and move all surrounding squares into it
-    # for the next states
-
+    for direction in ["up", "down", "left", "right"] do
+        new_rows = slide(direction)
+        states << Grid.new(@x, @y, new_rows) unless new_rows.nil?
+    end
+    states
   end
-
+  
+  def slide(direction)
+    x,y = @block_to_loc[Block.new(-1)]
+    puts "@rows: #{@rows}"
+    new_rows = Marshal::load(Marshal.dump(@rows))
+    puts "new_rows: #{new_rows}"
+    return_nil = true
+    unless is_invalid_move?(x, y, direction, new_rows)
+      return_nil = false
+      case direction
+      when "up"
+        puts "new_rows[y-1]: #{new_rows[y-1]}"
+        p "x: #{x}"
+        puts "new_rows[y-1][x]: #{new_rows[y-1][x]}"
+        new_rows[y][x] = new_rows[y-1][x]
+        puts "new_rows up 1: #{new_rows}"
+        new_rows[y-1][x] = Block.new(-1)
+        puts "new_rows up 2: #{new_rows}"
+      when "down"
+        new_rows[y][x] = new_rows[y+1][x]
+        new_rows[y+1][x] = Block.new(-1)
+      when "left"
+        new_rows[y][x] = new_rows[y][x-1]
+        puts "new_rows left 1: #{new_rows}"
+        new_rows[y][x-1] = Block.new(-1)
+        puts "new_rows left 2: #{new_rows}"
+      when "right"
+        new_rows[y][x] = new_rows[y][x+1]
+        puts "new_rows right 1: #{new_rows}"
+        new_rows[y][x+1] = Block.new(-1)
+        puts "new_rows right 2: #{new_rows}"
+      end
+    end
+    if return_nil == false
+      return new_rows 
+    else
+      return nil
+    end
+  end
+  
+  def is_invalid_move?(x, y, direction, new_rows)
+    return true if x == 0 and direction == "left"
+    return true if y == 0 and direction == "up"
+    return true if x == @x - 1 and direction == "right"
+    return true if y == @y - 1 and direction == "down"
+    return true if direction == "up" and y - 1 > 0 and new_rows[y-1][x] == Block.new(-1)
+    return true if direction == "down" and y < @y - 1 and new_rows[y+1][x] == Block.new(-1)
+    return true if direction == "left" and x - 1 > 0 and new_rows[y][x-1] == Block.new(-1)
+    return true if direction == "right" and x < @x - 1 and new_rows[y][x+1] == Block.new(-1)
+  end
+  
   def to_s
     @rows.each do |row|
       row.each do |col|
--- test/test_grid.rb
+++ test/test_grid.rb
@@ -13,8 +13,22 @@ class TestGrid < MiniTest::Unit::TestCase
     blocks << Block.new(6)
     blocks << Block.new(7)
     blocks << Block.new(8)
-    blocks << Block.new(nil)
+    blocks << Block.new(-1)
     @grid = Grid.new(3, 3, blocks)
+  end
+  
+  def alternate_grid
+    blocks = []
+    blocks << Block.new(-1)
+    blocks << Block.new(1)
+    blocks << Block.new(2)
+    blocks << Block.new(3)
+    blocks << Block.new(4)
+    blocks << Block.new(5)
+    blocks << Block.new(6)
+    blocks << Block.new(7)
+    blocks << Block.new(8)
+    @alternate_grid = Grid.new(3, 3, blocks)
   end
 
   def test_create_grid
@@ -27,7 +41,7 @@ class TestGrid < MiniTest::Unit::TestCase
     assert_equal @grid.block(2, 1), Block.new(6)
     assert_equal @grid.block(0, 2), Block.new(7)
     assert_equal @grid.block(1, 2), Block.new(8)
-    assert_equal @grid.block(2, 2), Block.new(nil)
+    assert_equal @grid.block(2, 2), Block.new(-1)
   end
 
   def test_out_of_bounds_block
@@ -43,7 +57,7 @@ class TestGrid < MiniTest::Unit::TestCase
   def test_next_state
     states = @grid.next_states
     states.each do |state|
-      refute states.block(2, 2).nil?
+      refute state.block(2, 2) == false
     end
   end
 
@@ -61,7 +75,7 @@ class TestGrid < MiniTest::Unit::TestCase
     blocks << Block.new(6)
     blocks << Block.new(7)
     blocks << Block.new(8)
-    blocks << Block.new(nil)
+    blocks << Block.new(-1)
     other_grid = Grid.new(3, 3, blocks)
     blocks2 = []
     blocks2 << Block.new(5)
@@ -72,7 +86,7 @@ class TestGrid < MiniTest::Unit::TestCase
     blocks2 << Block.new(6)
     blocks2 << Block.new(7)
     blocks2 << Block.new(8)
-    blocks2 << Block.new(nil)
+    blocks2 << Block.new(-1)
     other_grid2 = Grid.new(3, 3, blocks2)
     assert @grid == other_grid, "Grids are not equal when they should be."
     assert @grid.eql?(other_grid), "Grids are not equal when they should be."
@@ -92,4 +106,32 @@ class TestGrid < MiniTest::Unit::TestCase
   #  @grid.block(1, 2) == Block.new(8) and
   #  @grid.block(2, 2) == Block.new(nil)
   #end
+  
+  def test_is_invalid_move?
+    assert @grid.is_invalid_move?(2, 2, "down", @grid.rows), "Should be able to slide down from current position (0,0)"
+    assert @grid.is_invalid_move?(2, 2, "right", @grid.rows), "Should be able to slide right from current position (0,0)"
+    refute @grid.is_invalid_move?(2, 2, "up", @grid.rows), "Shouldn't be able to slide up from current position (0,0)"
+    refute @grid.is_invalid_move?(2, 2, "left", @grid.rows), "Shouldn't be able to slide left from current position (0,0)"
+    alternate_grid
+    assert @alternate_grid.is_invalid_move?(0, 0, "left", @alternate_grid.rows), "Should be able to slide left from current position (8,8)"
+    assert @alternate_grid.is_invalid_move?(0, 0, "up", @alternate_grid.rows), "Should be able to slide up from current position (8,8)"
+    refute @alternate_grid.is_invalid_move?(0, 0, "right", @alternate_grid.rows), "Shouldn't be able to slide right from current position (8,8)"
+    refute @alternate_grid.is_invalid_move?(0, 0, "down", @alternate_grid.rows), "Shouldn't be able to slide down from current position (8,8)"
+  end
+  
+  def test_slide
+    temp = @grid.slide("up")
+    up = Grid.new(3, 3, temp)
+    down = @grid.slide("down")
+    temp = @grid.slide("left")
+    left = Grid.new(3, 3, temp)
+    right = @grid.slide("right")
+    
+    refute_nil up, "up should not be nil."
+    assert_equal Block.new(-1), up.block(2, 1), "Should be able to slide nil up."
+    assert_nil down, "Should not be able to slide nil down."
+    refute_nil left, "left should not be nil."
+    assert_equal Block.new(-1), left.block(1, 2), "Should be able to slide nil left."
+    assert_nil right, "Should not be able to slide nil right."
+  end
 end
